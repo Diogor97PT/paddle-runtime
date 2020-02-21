20b9b8e40f5e4cbefbdbac52e28c2463eea9d573
Diogo Sousa <darsa@iscte-iul.pt>
2020-02-21 19:34:56 +0000

    Added an extra test for Fixed Value

--- pt.iscte.paddle.runtime/src/pt/iscte/paddle/runtime/tests/FixedValueTest.java
+++ pt.iscte.paddle.runtime/src/pt/iscte/paddle/runtime/tests/FixedValueTest.java
@@ -9,10 +9,10 @@ import org.junit.Test;
 
 import pt.iscte.paddle.model.IBlock;
 import pt.iscte.paddle.model.ILoop;
-import pt.iscte.paddle.model.IModel2CodeTranslator;
 import pt.iscte.paddle.model.IModule;
 import pt.iscte.paddle.model.IProcedure;
 import pt.iscte.paddle.model.IVariable;
+import pt.iscte.paddle.roles.IVariableRole;
 import pt.iscte.paddle.runtime.roles.IFixedValue;
 
 public class FixedValueTest {
@@ -43,11 +43,50 @@ public class FixedValueTest {
 		
 		body.addReturn(array);
 		
-		String code = module.translate(new IModel2CodeTranslator.Java());
-		System.out.println(code);
+		/*String code = module.translate(new IModel2CodeTranslator.Java());
+		System.out.println(code);*/
 		
 		assertEquals(IFixedValue.isFixedValue(n), true);
 		assertEquals(IFixedValue.isFixedValue(array), true);
+		assertEquals(IFixedValue.isFixedValue(i), false);
+		
+		IVariableRole vrn = IFixedValue.createFixedValue(n);
+		IVariableRole vrarray = IFixedValue.createFixedValue(array);
+		
+		assertEquals(vrn.toString(), vrn.getName());
+		assertEquals(vrarray.toString(), vrarray.getName() + " array that has been modified");
+	}
+	
+	@Test
+	public void fixedValueTestArrayChange() {
+		IModule module = IModule.create();				//Criar classe
+		module.setId("FixedValueTest");					//dar nome à classe
+		
+		IProcedure naturals = module.addProcedure(INT.array().reference());	//
+		naturals.setId("naturals");
+		
+		IVariable n = naturals.addParameter(INT);		//Parâmetro da Função
+		n.setId("n");
+		
+		IBlock body = naturals.getBody();				//corpo da função
+		
+		IVariable array = body.addVariable(INT.array().reference());
+		array.setId("array");
+		body.addAssignment(array, INT.array().heapAllocation(n));
+		
+		IVariable i = body.addVariable(INT, INT.literal(0));
+		i.setId("i");
+		
+		ILoop loop = body.addLoop(SMALLER.on(i, n));
+		loop.addArrayElementAssignment(array, ADD.on(i, INT.literal(1)), i);
+		loop.addAssignment(i, ADD.on(i, INT.literal(1)));
+		
+		body.addAssignment(array, INT.array().heapAllocation(n));
+		
+		body.addReturn(array);
+		
+		assertEquals(IFixedValue.isFixedValue(n), true);
+		assertEquals(IFixedValue.isFixedValue(array), false);
 		assertEquals(IFixedValue.isFixedValue(i), false);
 	}
 
